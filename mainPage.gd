aa37bb023d1952832935829e6dc66da9fb8eaa52
shashank <shashank.shashank.c@gmail.com>
2020-03-28 20:26:38 +0530

    changed proj layout and added bars

--- mainPage.gd
+++ mainPage.gd
@@ -1,31 +1,35 @@
 extends Node
 
 export (NodePath) var questionbankpath
+
 onready var questionbanknode = get_node(questionbankpath)
+
 var answer
 var questionnode
-var changeH  = 5
-var changeM  = 5
-var changeF  = 5
+var changeH  = 20
+var changeM  = 50
+var changeF  = 50
 
+func update_resource_ui():
+	$ResourcesPanel/HealthResourceBar/Label.text = "health: " + str(changeH)
+	$ResourcesPanel/HealthResourceBar/ProgressBar.value = changeH
+	$ResourcesPanel/MoneyResourceBar/Label.text = "money: " + str(changeM)
+	$ResourcesPanel/MoneyResourceBar/ProgressBar.value = changeM
+	$ResourcesPanel/SanityResourceBar/Label.text = "sanity: " + str(changeF)
+	$ResourcesPanel/SanityResourceBar/ProgressBar.value = changeF
 
 func _ready():
-	$health.text="health"+str(changeH)
-	$food.text="food"+str(changeF)
-	$money.text="money"+str(changeM)
-	
+	update_resource_ui()
 	randomize();
 	print(randi()%5)
 	setQuestion()
-	
 	
 func setQuestion():
 	questionnode=questionbanknode.get_child(randi()%5)
 	var questiontext = questionnode.fact
 	print(questiontext)
-	$Panel/Label.text=questiontext
+	$QuestionPanel/Label.text = questiontext
 	answer = questionnode.answer
-	
 	
 func updateStats(decision):
 	if decision == true:
@@ -37,12 +41,8 @@ func updateStats(decision):
 		changeH += questionnode.healthno
 		changeM += questionnode.moneyno
 		changeF += questionnode.foodno
-			
-	$health.text="health"+str(changeH)
-	$food.text="food"+str(changeF)
-	$money.text="money"+str(changeM)	
 	
-
+	update_resource_ui()
 
 func _on_Button_pressed():
 	updateStats(false)
@@ -50,9 +50,8 @@ func _on_Button_pressed():
 		print("you are correct!\n")
 
 	else:
-		print("you are wrong\n")	
-		
-	setQuestion();	
+		print("you are wrong\n")
+	setQuestion()
 
 
 func _on_Button2_pressed():
@@ -62,7 +61,7 @@ func _on_Button2_pressed():
 	else:
 		print("you are wrong\n")
 	
-	setQuestion();
+	setQuestion()
 	
 
 		
